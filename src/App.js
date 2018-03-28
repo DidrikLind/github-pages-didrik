0cda8289fb4c950464c33b07607692155a878766
didrik <didrik@didriks-MBP.lan>
2018-03-28 23:20:40 +0200

    add test text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React. Test deploy</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>Test deploy
         </p>
       </div>
     );
