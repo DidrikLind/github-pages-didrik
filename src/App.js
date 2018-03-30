270d613040e9554117bfc66406740678f8f1e866
didrik <didrik@didriks-MacBook-Pro.local>
2018-03-30 02:21:55 +0200

    Add test of material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import DrawerNav from './components/DrawerNav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React. Test deploy</h1>
-        </header>
-        <p className="App-intro">
-          <code>src/App.js</code>Test deploy
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <DrawerNav/>
+      </MuiThemeProvider>
     );
   }
 }
